1cea17394103668a61f045a0da913993de014020
hasansharif819 <hs.sharif819@gmail.com>
2022-09-24 23:29:05 +0600

    Edited

--- src/pages/Home/BannerProducts/BannerProduct.js
+++ src/pages/Home/BannerProducts/BannerProduct.js
@@ -17,7 +17,7 @@ const BannerProduct = ({ product }) => {
     </figure>
     <div className="card-body items-center text-center" id='productBody'>
     <h2 className="card-title text-red-800 font-bold text-2xl">{name}</h2>
-    <h2 className="card-title text-red-800">Price: ${price}</h2>
+    <h2 className="card-title text-red-800">Price: {price} tk</h2>
     <h2 className="card-title text-green-500">Available: {quantity}</h2>
     <div className="card-actions">
       <Link to={`/products/${_id}`}><button onClick={() => navigateToDetail(_id)} className="btn bg-red-900 btn-sm bg-gradient-to-r from-orange-500 to-red-400" id='buttonID'>PURCHASE</button></Link>
--- src/pages/Products/Product/Product.js
+++ src/pages/Products/Product/Product.js
@@ -19,7 +19,7 @@ const Product = ({ product }) => {
     </figure>
     <div className="card-body items-center text-center">
     <h2 className="card-title text-red-800 font-bold text-2xl">{name}</h2>
-    <h2 className="card-title text-red-900">Price: ${price}</h2>
+    <h2 className="card-title text-red-900">Price: {price} tk</h2>
     <h2 className="card-title text-green-500">Available: {quantity}</h2>
     <div className="card-actions">
       <Link to={`/products/${_id}`}><button onClick={() => navigateToDetail(_id)} className="btn bg-red-900 btn-sm mb-5 bg-gradient-to-r from-orange-500 to-red-400" id='buttonID'>PURCHASE</button></Link>
