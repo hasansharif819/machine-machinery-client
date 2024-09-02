22c089ed24325f819f6d0c8980a10743adfb8611
hasansharif819 <hs.sharif819@gmail.com>
2024-09-03 02:05:17 +0600

    responsive

--- src/pages/Home/Slider/Slider.js
+++ src/pages/Home/Slider/Slider.js
@@ -9,34 +9,32 @@ import './Slider.css';
 const Slider = () => {
     return (
         <div className='outside bg-base-300'>
-            <div className="hero min-h-screen large">
-<div className="hero min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={b9} className="rounded-lg shadow-2xl" alt='' id='largeImage'/>
-                <div className='text-white'>
-                    <h1 className="text-5xl font-bold">HELLO TOOLS</h1>
-                    <p className="py-6">We provide different types of tools and products all over the world with 100% qualityfull for wholesell</p>
-                    <Link to='/products'><button className="btn bg-gradient-to-r from-orange-500 to-red-400" id='buttonID'>EXPLORE</button>
-                    </Link>
+            <div className="hero min-h-[40vh] lg:min-h-[50vh]">
+                <div className="hero min-h-[30vh] lg:min-h-screen">
+                    <div className="hero-content flex-col lg:flex-row-reverse">
+                        <img src={b9} className="rounded-lg shadow-2xl" alt='' id='largeImage' />
+                        <div className='text-white'>
+                            <h1 className="text-4xl lg:text-5xl font-bold">HELLO TOOLS</h1>
+                            <p className="py-6">We provide different types of tools and products all over the world with 100% quality for wholesale.</p>
+                            <Link to='/products'>
+                                <button className="btn bg-gradient-to-r from-orange-500 to-red-400" id='buttonID'>EXPLORE</button>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
 
-            </div>
-            
             <div className="small">
                 <div className='inner2'>
-                    <img height={200} src={b1} alt="" />
-
+                    <img height={150} src={b1} alt="" />
                 </div>
                 <div className='inner3'>
-                    <img height={190} src={b2} alt="" />
+                    <img height={140} src={b2} alt="" />
                 </div>
                 <div className='inner4'>
-                    <img height={180} src={b3} alt="" />
+                    <img height={130} src={b3} alt="" />
                 </div>
             </div>
-
         </div>
     );
 };
